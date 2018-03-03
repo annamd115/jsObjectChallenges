2bf3896dc7662be42c3aac95f05188f5ef16e65d
Anna Dolan <anna.dolan115@gmail.com>
2018-03-03 14:05:50 -0600

    working on for loop

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("hi")
+//____________________________CHALLENGE1____________________________//
+
+var players = [
+    { 
+        name: "Bob", 
+        scores: [10, 65] 
+    }, 
+    { 
+        name: "Bill", 
+        scores: [90, 5] 
+    }, 
+    { 
+        name: "Charlie", 
+        scores: [40, 55] 
+    }
+]; // Displays "Bill"
+
+// var players = [
+//     { 
+//         name: "Bob", 
+//         scores: [15, 20] 
+//     }, 
+//     { 
+//         name: "Bill", 
+//         scores: [10, 15] 
+//     }, 
+//     { 
+//         name: "Charlie", 
+//         scores: [90, 10] 
+//     }
+// ]; // Displays "Charlie"
+
+for(i = 0; i < players.length; i++){
+    var playerString = "";
+    playerString += '<div class="player">';
+    playerString += '<h1> + players[i].name + </h1>';
+    playerString += '<h3> + players[i].scores + </h3>;
+    playerString += '</div>';
+    console.log(playerString);
+}
+
