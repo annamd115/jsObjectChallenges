284c9ce449182a69d4b18c08155c34aa5e0f34c9
Anna Dolan <anna.dolan115@gmail.com>
2018-03-10 16:40:09 -0600

    challenge1 complete

--- main.js
+++ main.js
@@ -15,27 +15,39 @@ var players = [
     }
 ]; // Displays "Bill"
 
-// var players = [
-//     { 
-//         name: "Bob", 
-//         scores: [15, 20] 
-//     }, 
-//     { 
-//         name: "Bill", 
-//         scores: [10, 15] 
-//     }, 
-//     { 
-//         name: "Charlie", 
-//         scores: [90, 10] 
-//     }
-// ]; // Displays "Charlie"
+var div1 = document.getElementById("challenge-1");
+var winningScore = 0;
+var winnerName = [];
 
-for(i = 0; i < players.length; i++){
-    var playerString = "";
-    playerString += '<div class="player">';
-    playerString += '<h1> + players[i].name + </h1>';
-    playerString += '<h3> + players[i].scores + </h3>;
-    playerString += '</div>';
-    console.log(playerString);
+// Loop through players array
+for(var i = 0; i < players.length; i++){
+    //to find the score of that player
+    var playerScores = players[i].scores;
+    var sumScore = 0;
+    //to loop through
+    for(var j = 0; j < playerScores.length; j++){
+        //and sum the scores
+        sumScore += playerScores[j];
+    }
+    //get rid of any scores over 100
+    if(sumScore > 100){
+        winner = false;
+        //compare the player's sumScore to existing winningScore
+    } else if(sumScore > winningScore){
+        //need to have locally scoped winnerName variable to only print first name in a tie
+        winnerName = [];
+        //if players[i].scores sum is greater than current winningScore, replace with sumScore
+        winningScore = sumScore;
+        //pushes to locally scoped array, will only add one name to global variable
+        winnerName.push(players[i].name);
+    }
 }
+
+div1.innerHTML = "<h3> Challenge1: </h3>" + winnerName;
+
+
+
+
+
+
 
